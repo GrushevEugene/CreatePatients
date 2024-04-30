79ac47fb73280e56e47dccd6f37663e4f2bd3a80
Evgeniy Grushev <evgeniy.grushev@techsourcing.by>
2024-04-30 22:55:13 +0300

    Add project files.

---
+++ ConsoleApp1/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public partial class Patient
+    {
+        public Guid Id { get; set; }
+        public Name? Name { get; set; }
+        public string? Gender { get; set; }
+        public DateTime Birthdate { get; set; }
+        public bool? Active { get; set; }
+        public Guid? NameId { get; set; }
+    }
+
+    public partial class Name
+    {
+        public Guid Id { get; set; }
+        public string? Use { get; set; }
+        public string? Family { get; set; }
+        public string[]? Given { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System.Text;
+using System.Text.Json;
+Names names = new Names();
+var random = new Random();
+HttpClient client = new HttpClient();
+string apiUrl = "https://localhost:7272/Patient";
+DateTime start = new DateTime(1995, 1, 1);
+int range = (DateTime.Today - start).Days;
+for (int i = 0; i < names.namesArr.Length; i++)
+{
+    var name = names.GetName(i);
+    var patient =
+        new Patient()
+        {
+            Id = Guid.NewGuid(),
+            Active = random.Next(2) == 1,
+            Birthdate = start.AddDays(random.Next(range)),
+            Gender = names.gender.GetValueOrDefault(random.Next(3)),
+            Name = new Name()
+            {
+                Id = Guid.NewGuid(),
+                Family = name[0],
+                Given = new string[] { name[1], name[2] },
+                Use = "official",
+            }
+        };
+    string response = await SendApiPostRequest(apiUrl, JsonSerializer.Serialize(patient));
+
+    Console.WriteLine($"Ответ API (итерация {i + 1}): {response}");
+}
+
+async Task<string> SendApiPostRequest(string url, string jsonContent)
+{
+    try
+    {
+        HttpContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url)
+        {
+            Content = content
+        };
+
+        HttpResponseMessage response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        return responseBody;
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("\nException Caught!");
+        Console.WriteLine("Message :{0} ", e.Message);
+        return null;
+    }
+}
